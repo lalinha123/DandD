90cf8114c6d69f1b8370bfe4f42bf7c87b0ae34e
Laís Gonçalves <laisgoncalves2109@gmail.com>
2023-01-28 18:27:37 -0200

    remaking everything!!!

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ let session = '';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
 
 
 // -------------------------------------------------------------------
@@ -40,115 +39,91 @@ app.use(sessions ({
     resave: false,
 }));
 
+const startSession = (req, data) => {
+    session = null;
+    session = req.session;
+    session.userid = data.id;
+    session.user = data;
+}
+
 
 // -------------------------------------------------------------------
 // APP ROUTERS
 app.get('/', (req, res) => {
     if (session) {
-        res.render('index', {user: session.user, session: session});
+        res.render('index', {session: session});
     } else {
-        res.render('index', {user: null, session: null});
+        res.render('index', {session: null});
     }
 });
 
-
-// LOG IN
+// LOGIN
 app.get('/login', (req, res) => {
-    if (session) {
-        res.redirect('/');
-    } else {
-        res.render('login', {msg: ''});
-    }
+    res.render('login', {msg: null});
 });
 
 app.post('/login', (req, res) => {
-    const username_in = req.body.username;
-    const password_in = req.body.password;
+    const nickname = req.body.nickname;
+    const password = req.body.password;
 
-    const render = (msg) => {
-        res.render('login', {msg: msg});
-    }
-
-    db.get('SELECT * FROM users WHERE username = $username', 
-    {$username: username_in}, (err, userdata) => {
-
-        if (!username_in || !password_in) {
-            render('Please, fill all the blank areas.');
-        } else if (!userdata) {
-            render('This user does not exist.');
-
-        } else if (userdata.password === password_in) {
-            session = req.session;
-            session.userid = userdata.id;
-            session.user = userdata;
-            res.redirect('/');
-
-        } else if (userdata.password !== password_in) {
-            render('Invalid password');
-
-        } else {
-            render('Some error occured!');
-        }
-    });
-});
-
-
-// REGISTER
-app.get('/register', (req, res) => {
-    if (session) {
-        res.redirect('/');
+    if (nickname && password) {
+        db.get('SELECT * FROM users WHERE nickname = $nickname AND password = $password', 
+        {$nickname: nickname, $password: password}, (err, userdata) => {
+            if (userdata) {
+                startSession(req, userdata);
+                res.redirect('/');
+            } else {
+                res.render('login', {msg: 'Invalid user or password!'});
+            }
+        });
     } else {
-        res.render('register', {msg: ''});
+        res.render('login', {msg: 'Pls fill all the blank areas'});
     }
 });
 
-app.post('/register', (req, res) => {
-    const username_in = req.body.username;
-    const password_in = req.body.password;
-
-    db.get('SELECT id FROM users ORDER BY id DESC LIMIT 1', (err, row) => {
-        const id = row ? Number(row.id) + 1 : 1;
-
-        if (!username_in || !password_in) {
-            res.render('register', {msg: 'Please, fill all the blank areas.'});
-        } else {
-            db.get('SELECT username FROM users WHERE username = $username', 
-            { $username: username_in }, (err, row_name) => {
-                if (!row_name) {
-                    db.run('INSERT INTO users (id, username, password) VALUES ($id, $username, $password)',
-                    {
-                        $id: id,
-                        $username: username_in,
-                        $password: password_in
-                    }, () => {
-                        db.get('SELECT * FROM users WHERE username = $username',
-                        {$username: username_in}, (err, userdata) => {
-                            if  (!err) {
-                                session = req.session;
-                                session.userid = userdata.id;
-                                session.user = userdata;
-                                res.redirect('/');
-                            }
-                        });
-                    });
-                } else {
-                    res.render('register', {msg: 'This username has been already used.'});
-                }
-            });
-
-        }
-    });
+// SIGN UP
+app.get('/signup', (req, res) => {
+    res.render('signup', {msg: null});
 });
 
+app.post('/signup', (req, res) => {
+    const nickname = req.body.nickname;
+    const password = req.body.password;
+
+    if (nickname && password) {
+        db.get('SELECT nickname FROM users WHERE nickname = $nickname', 
+        {$nickname: nickname}, (err, data) => {
+            if (!data) {
+                db.get('SELECT id FROM users ORDER BY id DESC LIMIT 1', (err, iddata) => {
+                    const id = iddata ? iddata.id++ : 1;
+                    db.run('INSERT INTO users (id, nickname, password) VALUES ($id, $nickname, $password)',
+                    {$id: id, $nickname: nickname, $password: password}, (err) => {
+                        if (!err) {
+                            startSession(req, {
+                                id: id,
+                                nickname: nickname,
+                                password: password
+                            });
+                            res.redirect('/');
+                        }
+                    });
+                });
+            } else {
+                res.render('signup', {msg: 'This user already exists! :O'});
+            }
+        });
+    } else {
+        res.render('signup', {msg: 'Pls fill all the blank areas'});
+    }
+});
 
 //LOG OUT
 app.get('/logout', (req, res) => {
-    req.session.destroy(function(err){
+    req.session.destroy(() => {
         session = null;
         res.redirect('/');
     });
 });
-
 
 // -------------------------------------------------------------------
 app.listen(port, () => {
